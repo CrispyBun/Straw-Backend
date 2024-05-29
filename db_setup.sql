b3f4c0416caa677286018c3cf0eec361d640e31c
CrispyBun <74562111+CrispyBun@users.noreply.github.com>
2024-05-29 18:22:37 +0200

    Update db_setup.sql

--- db_setup.sql
+++ db_setup.sql
@@ -29,7 +29,8 @@ CREATE TABLE "post" (
     "id" SERIAL NOT NULL,
     "author" int NOT NULL,
     "board" int NOT NULL,
-    "content" text,
+    "text_content" text,
+    "attachment_content" text,
     PRIMARY KEY ("id"),
     FOREIGN KEY ("board") REFERENCES "board" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
     FOREIGN KEY ("author") REFERENCES "user" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
