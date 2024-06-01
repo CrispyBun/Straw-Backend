3ec62589674b2fc81e1112cdb0bcf195abd2ebee
CrispyBun <74562111+CrispyBun@users.noreply.github.com>
2024-06-01 02:41:18 +0200

    Change request log level do debug

--- src/middleware/logRequest.ts
+++ src/middleware/logRequest.ts
@@ -2,7 +2,7 @@ import * as express from 'express';
 import { logger, requestLogger } from '../logger/loggers';
 
 const logRequest = (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    logger.verbose(`[<- ${res.logId}] Got request: ${req.method} ${req.url}`);
+    logger.debug(`[<- ${res.logId}] Got request: ${req.method} ${req.url}`);
     requestLogger.info({req: req, res: res});
     next();
 }
